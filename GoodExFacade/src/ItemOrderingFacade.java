0439b7b74cf63d2390a871aa65fdb51f359beb78
ZiitoZnarf <126751084+ZiitoZnarf@users.noreply.github.com>
2024-12-02 11:21:16 -0700

    Finished the good Ex for Facade

--- GoodExFacade/src/ItemOrderingFacade.java
+++ GoodExFacade/src/ItemOrderingFacade.java
@@ -1,5 +1,91 @@
+import java.util.ArrayList;
 
 public class ItemOrderingFacade {
     private Store theStore;
+    private ArrayList<Account> accounts;
 
+    public ItemOrderingFacade() {
+        theStore = new Store();
+        accounts = new ArrayList<>();
+    }
+
+    private void addAccount(String name) {
+        accounts.add(new Account(name));
+    }
+
+    public boolean orderItem(String itemName, int quantity, String accountName) {
+        Account theAccount = getAccountByName(accountName);
+
+        if (theAccount == null) {
+            addAccount("Julian");
+            theAccount = getAccountByName(accountName);
+        }
+
+        //Get All Store Entries
+        ArrayList<StoreEntry> entryList = theStore.getStoreEntries();
+
+        //Find the ID for the given store entry
+        StoreEntry itemRef = null;
+
+        for (int i = 0; i < entryList.size(); i++) {
+            if (entryList.get(i).getName().equals(itemName)) {
+                itemRef = entryList.get(i);
+                break;
+            }
+        }
+
+        //Make sure the Item exists
+        if (itemRef == null) {
+            System.out.println("Error: Item not found");
+            return false;
+        }
+
+        //Make sure the item has a high enough quantity to be purchased
+        if (itemRef.getQuantity() < quantity) {
+            System.out.println("Error: Item stock too low");
+            return false;
+        }
+
+        //Make sure the account has enough available funds
+        if (theAccount.getBalance() < (itemRef.getPrice() * quantity)) {
+            System.out.println("Error: Insufficient account funds");
+            return false;
+        }
+
+        //Subtract the Accounts available funds
+        theAccount.setBalance(theAccount.getBalance() - (itemRef.getPrice() * quantity));
+
+        //Subtract the value from the stores quantity
+        itemRef.setQuantity(itemRef.getQuantity() - quantity);
+
+        //Create order in the store orderHistory
+        theStore.getOrderHistory().add(new Order(theAccount.getName(), itemRef.getId(),
+                quantity, quantity * itemRef.getPrice()));
+
+        //Add the items to the account inventory
+        for (int i = 0; i < quantity; i++) {
+            theAccount.addItem(new Item(itemName));
+        }
+
+
+        System.out.println("Success: Items purchased and added to account\n");
+        displayAccountInventory(accountName);
+        return true;
+    }
+
+    private Account getAccountByName(String name) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (name.equals(accounts.get(i).getName())) {
+                return accounts.get(i);
+            }
+        }
+
+        return null;
+    }
+
+    private void displayAccountInventory(String name) {
+        Account acc = getAccountByName(name);
+        if (acc != null)
+            acc.displayInventory();
+    }
 }
--- GoodExFacade/src/Main.java
+++ GoodExFacade/src/Main.java
@@ -1,60 +1,19 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        Store theStore = new Store();
+        ItemOrderingFacade storeInterface = new ItemOrderingFacade();
 
-        Account theAccount = new Account("Julian");
+
+        String accountName = "Julian";
         String itemName = "Bandage";
         int itemQuantity = 3;
 
-        theAccount.displayInventory();
 
-        //Get All Store Entries
-        ArrayList<StoreEntry> entryList = theStore.getStoreEntries();
+        //One Single Allowed Command
+        //Much less freedom of how to execute things, but much easier to do
+        storeInterface.orderItem(itemName, itemQuantity, accountName);
 
-        //Find the ID for the given store entry
-        StoreEntry itemRef = null;
 
-        for (int i = 0; i < entryList.size(); i++) {
-            if (entryList.get(i).getName().equals(itemName)) {
-                itemRef = entryList.get(i);
-                break;
-            }
-        }
 
-        //Make sure the Item exists
-        if (itemRef == null) {
-            return;
-        }
-
-        //Make sure the item has a high enough quantity to be purchased
-        if (itemRef.getQuantity() < itemQuantity) {
-            return;
-        }
-
-        //Make sure the account has enough available funds
-        if (theAccount.getBalance() < (itemRef.getPrice() * itemQuantity)) {
-            return;
-        }
-
-        //Subtract the Accounts available funds
-        theAccount.setBalance(theAccount.getBalance() - (itemRef.getPrice() * itemQuantity));
-
-        //Subtract the value from the stores quantity
-        itemRef.setQuantity(itemRef.getQuantity() - itemQuantity);
-
-        //Create order in the store orderHistory
-        theStore.getOrderHistory().add(new Order(theAccount.getName(), itemRef.getId(),
-                itemQuantity, itemQuantity * itemRef.getPrice()));
-
-        //Add the items to the account inventory
-        for (int i = 0; i < itemQuantity; i++) {
-            theAccount.addItem(new Item(itemName));
-        }
-
-        //Show newly added inventory item
-        theAccount.displayInventory();
 
     }
 }
